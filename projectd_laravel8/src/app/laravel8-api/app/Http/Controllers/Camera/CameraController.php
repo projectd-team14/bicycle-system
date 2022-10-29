cfd4cba38b9411e6366813bd606afc61d0f48eaa
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-10-30 07:38:06 +0900

    urlをローカルに修正。

--- projectd_laravel8/src/app/laravel8-api/app/Http/Controllers/Camera/CameraController.php
+++ projectd_laravel8/src/app/laravel8-api/app/Http/Controllers/Camera/CameraController.php
@@ -58,7 +58,8 @@ class CameraController extends Controller
         } else {
            Camera::where('cameras_id', $id)->update(['cameras_status'=>'Run']);
            //PythonAPI
-           $url = "https://projectd-fastapi.herokuapp.com/detect/?id=${id}";
+           // $url = "https://projectd-fastapi.herokuapp.com/detect/?id=${id}";
+           $url = "host.docker.internal:9000/detect/?id=${id}";
            $conn = curl_init();
            curl_setopt($conn, CURLOPT_URL, $url);
            curl_setopt($conn, CURLOPT_RETURNTRANSFER, true);
