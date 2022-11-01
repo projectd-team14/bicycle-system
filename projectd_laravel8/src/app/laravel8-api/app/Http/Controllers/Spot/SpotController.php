b96ef87fb78490aed54fa34b45ff53d4cf98f45e
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-11-01 16:46:54 +0900

    postする内容を変更。

--- projectd_laravel8/src/app/laravel8-api/app/Http/Controllers/Spot/SpotController.php
+++ projectd_laravel8/src/app/laravel8-api/app/Http/Controllers/Spot/SpotController.php
@@ -32,38 +32,24 @@ class SpotController extends Controller
         $insertLong = (string) $xml->coordinate->lng;
         $insertLat= (string) $xml->coordinate->lat;
         if ($data['spots_url'] == "") {
-            $spotId = Spot::insertGetId([
-                'spots_name' => $data['spots_name'],
-                'users_id' => $id, 
-                'spots_longitude' => $insertLong, 
-                'spots_latitude' => $insertLat,
-                'spots_address' => $data['spots_address'],
-                'spots_status' => 'None',
-                'spots_count_day1' => 'None',
-                'spots_count_week1' => 'None',
-                'spots_count_month1' => 'None',
-                'spots_count_month3' => 'None',
-                'spots_max' => 100,
-                'spots_over_time' => 3600,
-                'spots_url' => '画像のパスが入ります',
-            ]);            
-        } else {
-            $spotId = Spot::insertGetId([
-                'spots_name' => $data['spots_name'],
-                'users_id' => $id, 
-                'spots_longitude' => $insertLong, 
-                'spots_latitude' => $insertLat,
-                'spots_address' => $data['spots_address'],
-                'spots_status' => 'None',
-                'spots_count_day1' => 'None',
-                'spots_count_week1' => 'None',
-                'spots_count_month1' => 'None',
-                'spots_count_month3' => 'None',
-                'spots_max' => 100,
-                'spots_over_time' => 3600,
-                'spots_url' => $data['spots_url'],
-            ]); 
-        };
+        $spotId = Spot::insertGetId([
+             'spots_name' => $data['spots_name'],
+             'users_id' => $id, 
+             'spots_longitude' => $insertLong, 
+             'spots_latitude' => $insertLat,
+             'spots_address' => $data['spots_address'],
+             'spots_status' => 'None',
+             'spots_count_day1' => 'None',
+             'spots_count_week1' => 'None',
+             'spots_count_month1' => 'None',
+             'spots_count_month3' => 'None',
+             'spots_max' => 100,
+             'spots_over_time' => 3600,
+             'spots_url' => $data['spots_url'],
+        ]);            
+        }
+
+
         return $data;
     }
 
