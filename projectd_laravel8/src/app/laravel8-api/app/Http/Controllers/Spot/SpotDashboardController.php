fb2ad6b4e4cc79b35a8ee61923531016e7814cdf
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-11-22 02:39:15 +0900

    ダッシュボード用のコントローラーのを追加。

---
+++ projectd_laravel8/src/app/laravel8-api/app/Http/Controllers/Spot/SpotDashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\コン�Spot;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SpotDashboardController extends Controller
+{
+    //
+}
