1f43e0e07bd2c19a88b93092ec2f9a02490f93ad
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-12-17 19:24:56 +0900

    設定にSSRを明記
    デフォルト設定が未定義だったため修正

--- projectd_nuxt3/src/nuxt.config.ts
+++ projectd_nuxt3/src/nuxt.config.ts
@@ -2,6 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: true,
+  target: "server",
   runtimeConfig: {
     public:{
       LaravelURL: process.env.LARAVEL_URL,
