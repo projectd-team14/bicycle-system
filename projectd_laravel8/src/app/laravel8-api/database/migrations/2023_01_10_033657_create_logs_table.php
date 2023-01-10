a8f0a52dc5c501a6d5317db7bbb6412c2933de11
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-01-10 12:41:19 +0900

    ログ記録用のマイグレーションファイルを追加

---
+++ projectd_laravel8/src/app/laravel8-api/database/migrations/2023_01_10_033657_create_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('spots_id')->index();
+            $table->integer('cameras_id')->index();
+            $table->string('logs_status')->defalut('None');
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
